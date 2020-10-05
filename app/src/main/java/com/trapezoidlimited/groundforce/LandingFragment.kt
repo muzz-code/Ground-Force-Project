7cbfbd8285bcf3991aaced64ac1bbd9934b35db5
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-05 23:51:18 +0100

    Completed landing page

--- app/src/main/java/com/trapezoidlimited/groundforce/LandingFragment.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/LandingFragment.kt
@@ -6,12 +6,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.trapezoidlimited.groundforce.databinding.FragmentLandingBinding
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
 /**
  * A simple [Fragment] subclass.
  * Use the [LandingFragment.newInstance] factory method to
@@ -32,7 +26,6 @@ class LandingFragment : Fragment() {
         // Inflate the layout for this fragment
         return view
     }
-
 
     override fun onDestroy() {
         super.onDestroy()
