0e698d2c845ee661e82da2a2224d475b4ccbac09
kome <omodo.oghenekome@decagon.dev>
2020-10-05 20:18:02 +0100

    Onboarding screen completed

--- app/src/main/java/com/trapezoidlimited/groundforce/ui/main/MainActivity.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/main/MainActivity.kt
@@ -3,14 +3,20 @@ package com.trapezoidlimited.groundforce.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.trapezoidlimited.groundforce.R
+import com.trapezoidlimited.groundforce.databinding.ActivityMainBinding
 import dagger.hilt.EntryPoint
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
 
