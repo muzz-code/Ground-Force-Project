c847ffd2be5b06a42a83829ea1cef7bc1eb1943c
Victor Adewumi <VictorOluwaseun@users.noreply.github.com>
2020-10-06 08:46:12 +0100

    completed phone activation screen

--- app/src/main/java/com/trapezoidlimited/groundforce/ui/PhoneActivationFragment.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/PhoneActivationFragment.kt
@@ -1,10 +1,17 @@
 package com.trapezoidlimited.groundforce.ui
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.text.Spanned
+import android.text.TextPaint
+import android.text.method.LinkMovementMethod
+import android.text.style.ClickableSpan
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import com.trapezoidlimited.groundforce.R
 import com.trapezoidlimited.groundforce.databinding.FragmentPhoneActivationBinding
 
@@ -25,6 +32,43 @@ class PhoneActivationFragment : Fragment() {
         val view = binding.root
 
 
+//        <!--    47-63-->
+//        <!--    79 - 92-->
+
+        // Get Test from String Resource
+        val codeText = getText(R.string.phone_activ_t_and_c)
+        // Get an instance of SpannableString
+        val ssText = SpannableString(codeText)
+        // Implement ClickableSpan
+        val clickableSpan: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Toast.makeText(requireContext(), "Clicked!", Toast.LENGTH_LONG).show()
+            }
+            // Change color and remove underline
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = ContextCompat.getColor(requireContext(), R.color.colorTextResend)
+                ds.isUnderlineText = false
+            }
+        }
+
+        val clickableSpan1: ClickableSpan = object : ClickableSpan() {
+            override fun onClick(view: View) {
+                Toast.makeText(requireContext(), "Clicked!", Toast.LENGTH_LONG).show()
+            }
+            // Change color and remove underline
+            override fun updateDrawState(ds: TextPaint) {
+                super.updateDrawState(ds)
+                ds.color = ContextCompat.getColor(requireContext(), R.color.colorTextResend)
+                ds.isUnderlineText = false
+            }
+        }
+        // Set the span text
+        ssText.setSpan(clickableSpan, 47, 63, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        ssText.setSpan(clickableSpan1, 79, 92, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE)
+        // Make the text spannable and clickable
+        binding.phoneActivTermsCondition.text = ssText
+        binding.phoneActivTermsCondition.movementMethod = LinkMovementMethod.getInstance()
 
         // Inflate the layout for this fragment
         return view
