eaeb05f7bbf5b35cc00532bed043634571a3ca1f
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-10-13 21:21:48 +0100

    make phone verif confirm button nav to location verification

--- app/src/main/java/com/trapezoidlimited/groundforce/ui/PhoneVerificationFragment.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/PhoneVerificationFragment.kt
@@ -63,7 +63,12 @@ class PhoneVerificationFragment : Fragment() {
         binding.phoneActivArrowBackIv.setOnClickListener {
             findNavController().popBackStack()
         }
-        //
+        //Text location verification fragment
+        //click on phone verification to text
+        binding.phoneVerifConfirmBtn.setOnClickListener {
+            findNavController().navigate(R.id.locationVerificationFragment)
+        }
+
         requireActivity().onBackPressedDispatcher.addCallback{
             findNavController().popBackStack()
         }
