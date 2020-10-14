de7d91bff1f21f9a779448e04ebf05bed079f706
Daniel Ayodeji <danielayodeji97@gmail.com>
2020-10-14 17:32:14 +0100

    Added comment

--- app/src/main/java/com/trapezoidlimited/groundforce/ui/profile/CreateProfileFragmentOne.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/profile/CreateProfileFragmentOne.kt
@@ -1,7 +1,10 @@
 package com.trapezoidlimited.groundforce.ui.profile
 
+import android.Manifest.permission.CAMERA
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.app.DatePickerDialog
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.provider.MediaStore
@@ -11,7 +14,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.navigation.fragment.findNavController
 import com.trapezoidlimited.groundforce.R
 import com.trapezoidlimited.groundforce.databinding.FragmentCreateProfileOneBinding
@@ -28,6 +34,8 @@ class CreateProfileFragmentOne : Fragment(), AdapterView.OnItemSelectedListener 
     private lateinit var dateSetListener: DatePickerDialog.OnDateSetListener
 
     private lateinit var date : String
+
+    private val PERMISSION_REQUEST_CODE: Int = 101
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -101,9 +109,11 @@ class CreateProfileFragmentOne : Fragment(), AdapterView.OnItemSelectedListener 
         super.onActivityCreated(savedInstanceState)
         val dateButton = binding.fragmentCreateProfileOneDateBirthEt
 
+        /** Show the date button on click of date button **/
         dateButton.setOnClickListener {
             showDatePickerDialog(requireView())
         }
+
 
         dateSetListener = DatePickerDialog.OnDateSetListener { datePicker, year, month, day ->
             date = "${month+1}/$day/$year"
@@ -112,24 +122,62 @@ class CreateProfileFragmentOne : Fragment(), AdapterView.OnItemSelectedListener 
 
         val cameraButton = binding.fragmentCreateProfileOneIb
 
+        /** On click on camera button, check if permission is granted, if granted take picture or request other wise **/
         cameraButton.setOnClickListener {
-            try {
-                val intent = Intent()
-                intent.action = MediaStore.ACTION_IMAGE_CAPTURE
-                startActivity(intent)
-            } catch (e : Exception) {
-                e.printStackTrace()
-            }
-
-
+            if (checkPermission()) takePicture() else requestPermission()
         }
 
     }
 
 
+    /** Take picture function **/
+    private fun takePicture() {
+        try {
+            val intent = Intent()
+            intent.action = MediaStore.ACTION_IMAGE_CAPTURE
+            startActivity(intent)
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun checkPermission(): Boolean {
+        return (ContextCompat.checkSelfPermission(requireContext(), android.Manifest.permission.CAMERA) ==
+                PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(requireContext(),
+            android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+    }
+
+    private fun requestPermission() {
+        ActivityCompat.requestPermissions(requireActivity(), arrayOf(READ_EXTERNAL_STORAGE, CAMERA),
+            PERMISSION_REQUEST_CODE)
+    }
 
 
-    //Show Date Picker Dialog Function
+    /** On request permission result grant user permission or show a permission denied message **/
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+
+                if ((grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+
+                    takePicture()
+
+                } else {
+                    Toast.makeText(requireContext(), "Permission Denied", Toast.LENGTH_SHORT).show()
+                }
+                return
+            }
+
+            else -> {
+
+            }
+        }
+    }
+
+
+
+    /** Show Date picker Dialog Function **/
     @RequiresApi(Build.VERSION_CODES.M)
     private fun showDatePickerDialog(v: View) {
         // Use the current date as the default date in the picker
@@ -145,8 +193,5 @@ class CreateProfileFragmentOne : Fragment(), AdapterView.OnItemSelectedListener 
         dialog.show()
 
     }
-
-
-
 
 }
