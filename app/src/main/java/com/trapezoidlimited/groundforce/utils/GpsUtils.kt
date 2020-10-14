3669b310601546d4babcce0562db92a6c6375d16
sterlingking1990 <izundukingsleyemeka@gmail.com>
2020-10-14 18:55:32 +0100

    set app constants and gps utils

---
+++ app/src/main/java/com/trapezoidlimited/groundforce/utils/GpsUtils.kt
@@ -0,0 +1,74 @@
+package com.trapezoidlimited.groundforce.utils
+
+import android.app.Activity
+import android.content.Context
+import android.content.IntentSender.SendIntentException
+import android.location.LocationManager
+import android.util.Log
+import android.widget.Toast
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+
+
+class GpsUtils(context: Context) {
+    private val context: Context = context
+    private val mSettingsClient: SettingsClient = LocationServices.getSettingsClient(context)
+    private val mLocationSettingsRequest: LocationSettingsRequest
+    private val locationManager: LocationManager = context.getSystemService(Context.LOCATION_SERVICE) as LocationManager
+    private val locationRequest: LocationRequest = LocationRequest.create()
+
+    // method for turn on GPS
+    fun turnGPSOn(onGpsListener: OnGpsListener?) {
+        if (locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            onGpsListener?.gpsStatus(true)
+        } else {
+            mSettingsClient
+                .checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener((context as Activity)) { //  GPS is already enable, callback GPS status through listener
+                    onGpsListener?.gpsStatus(true)
+                }
+                .addOnFailureListener(
+                    (context as Activity)
+                ) { e ->
+                    when ((e as ApiException).statusCode) {
+                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                            // Show the dialog by calling startResolutionForResult(), and check the
+                            // result in onActivityResult().
+                            val rae = e as ResolvableApiException
+                            rae.startResolutionForResult(
+                                context as Activity,
+                                AppConstants.GPS_REQUEST
+                            )
+                        } catch (sie: SendIntentException) {
+                            Log.i("PendingIntentGPS", "PendingIntent unable to execute request.")
+                        }
+                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                            val errorMessage = "Location settings are inadequate, and cannot be " +
+                                    "fixed here. Fix in Settings."
+                            Log.e("GPSSettingsInadequate", errorMessage)
+                            Toast.makeText(context as Activity, errorMessage, Toast.LENGTH_LONG)
+                                .show()
+                        }
+                    }
+                }
+        }
+    }
+
+    interface OnGpsListener {
+        fun gpsStatus(isGPSEnable: Boolean)
+    }
+
+    init {
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = 10 * 1000.toLong()
+        locationRequest.fastestInterval = 2 * 1000.toLong()
+        val builder = LocationSettingsRequest.Builder()
+            .addLocationRequest(locationRequest)
+        mLocationSettingsRequest = builder.build()
+
+        //**************************
+        builder.setAlwaysShow(true) //this is the key ingredient
+        //**************************
+    }
+}
