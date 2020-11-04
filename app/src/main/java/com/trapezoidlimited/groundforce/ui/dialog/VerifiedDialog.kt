3f645e8cbaeccdde3d11c7c144b01699cdced018
kolanse <samuel.ochuba@decagon.dev>
2020-11-04 13:09:32 +0100

    solved location fragment and dialog bug

---
+++ app/src/main/java/com/trapezoidlimited/groundforce/ui/dialog/VerifiedDialog.kt
@@ -0,0 +1,43 @@
+package com.trapezoidlimited.groundforce.ui.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.DialogFragment
+import androidx.navigation.fragment.findNavController
+import com.trapezoidlimited.groundforce.R
+import com.trapezoidlimited.groundforce.databinding.VerificationResultPageBinding
+
+
+class VerifiedDialog : DialogFragment() {
+    private var _binding: VerificationResultPageBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dialog!!.window?.setBackgroundDrawableResource(R.drawable.round_corner);
+        _binding = VerificationResultPageBinding.inflate(inflater, container, false)
+
+        binding.locationVerifiedDialogTv.setOnClickListener {
+          findNavController().navigate(R.id.createPinFragment)
+            dismiss()
+        }
+        return binding.root
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val width = (resources.displayMetrics.widthPixels * 0.85).toInt()
+        val height = (resources.displayMetrics.heightPixels * 0.55).toInt()
+        dialog!!.window?.setLayout(width, height)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
--- app/src/main/java/com/trapezoidlimited/groundforce/utils/FragmentExtension.kt
+++ app/src/main/java/com/trapezoidlimited/groundforce/utils/FragmentExtension.kt
@@ -1,10 +1,15 @@
 package com.trapezoidlimited.groundforce.utils
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.content.DialogInterface
 import android.graphics.Color
 import android.view.View
 import android.view.Window
+import androidx.appcompat.app.AlertDialog
 import androidx.fragment.app.Fragment
 import com.trapezoidlimited.groundforce.R
+import com.trapezoidlimited.groundforce.ui.dialog.VerifiedDialog
 
 
 /**
@@ -41,3 +46,74 @@ fun Fragment.hideStatusBar(){
             or View.SYSTEM_UI_FLAG_FULLSCREEN)
 }
  */
+
+/**
+ * Fade out the view
+ */
+
+fun View.crossShow(duration: Long) {
+
+
+    // Animate the loading view to 0% opacity. After the animation ends,
+    // set its visibility to GONE as an optimization step (it won't
+    // participate in layout passes, etc.)
+    this.animate()
+        .alpha(1f)
+        .setDuration(duration)
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+
+                crossfade(duration)
+            }
+        })
+}
+
+/**
+ * Fade in the view
+ */
+
+fun View.crossfade(duration: Long) {
+
+
+    // Animate the loading view to 0% opacity. After the animation ends,
+    // set its visibility to GONE as an optimization step (it won't
+    // participate in layout passes, etc.)
+    this.animate()
+        .alpha(0f)
+        .setDuration(duration)
+        .setListener(object : AnimatorListenerAdapter() {
+            override fun onAnimationEnd(animation: Animator) {
+                crossShow(duration)
+            }
+        })
+}
+
+/**
+ * Inflate the dialog fragment that shows location has been verified
+ **/
+fun Fragment.onLogin() {
+    var frag = parentFragmentManager.beginTransaction()
+    VerifiedDialog().show(frag, "This")
+}
+
+/**
+ * Show Dialog
+ */
+fun Fragment.showAlertDialog(
+    message: Int,
+
+    listener: View.OnClickListener? = null
+) {
+    val dialogBuilder = AlertDialog.Builder(requireActivity())
+    dialogBuilder.setMessage(getString(message))
+        // if the dialog is cancelable
+        .setCancelable(false)
+        .setPositiveButton("Ok", DialogInterface.OnClickListener { dialog, id ->
+            listener
+
+        })
+
+    val alert = dialogBuilder.create()
+    alert.setTitle("Test")
+    alert.show()
+}
