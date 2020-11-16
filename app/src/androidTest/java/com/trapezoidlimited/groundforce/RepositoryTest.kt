bbe827fb16723f6c6b71e68ca8d0aab9027af8c5
Olamicode <oafagbemi@gmail.com>
2020-11-16 15:03:57 +0100

    refactored validation methods and implemented unit tests"

---
+++ app/src/androidTest/java/com/trapezoidlimited/groundforce/RepositoryTest.kt
@@ -0,0 +1,23 @@
+package com.trapezoidlimited.groundforce.repository
+
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+@HiltAndroidTest
+class RepositoryTest {
+    @get: Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun someTest() {
+        
+    }
+}
